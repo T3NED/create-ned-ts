69efffdcd9c2a54a480c31f722e5eb47253057bf
T3NED <oss@t3ned.dev>
2022-06-30 01:08:32 +0100

    Add package preset prompts

--- src/prompts/index.ts
+++ src/prompts/index.ts
@@ -1,3 +1,4 @@
 export * from "./base/input";
 export * from "./base/choice";
 export * from "./presets/sandbox";
+export * from "./presets/package";
---
+++ src/prompts/presets/package.ts
@@ -0,0 +1,15 @@
+import { PresetPromptResult } from "../interface";
+import { makeShared } from "../base/shared";
+import { boolean } from "../base/boolean";
+
+export const makePackage = async (): Promise<PresetPromptResult> => {
+	const githubPackageCD = await boolean("add github cd");
+	const shared = await makeShared();
+
+	return {
+		...shared,
+		additionalDependencies: [],
+		githubPackageCD,
+		package: true,
+	};
+};
